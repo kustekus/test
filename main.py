b37a5621461855e143142e23028038730fa83b48
kustekus <artdubrovin17@gmail.com>
2023-12-16 22:39:55 +0300

    Ввод чисел и список операций

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-a=int(input("Введите первое число: "))
-op=input("Введите операцию: ")
-b=int(input("Введите второе число: "))
-operations=["+","-","/","*"]
-
+while True:
+    operations = ["+", "-", "/", "*"]
+    a=int(input("Введите первое число: "))
+    print(f"Список операций:",end=" ")
+    print(*operations)
+    op=input("Введите операцию: ")
+    b=int(input("Введите второе число: "))
