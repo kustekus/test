8d78cea5a47936ea3ad51af792192379bc732199
kustekus <artdubrovin17@gmail.com>
2023-12-16 22:41:16 +0300

    Условия

--- main.py
+++ main.py
@@ -5,3 +5,15 @@ while True:
     print(*operations)
     op=input("Введите операцию: ")
     b=int(input("Введите второе число: "))
+
+    if op == operations[0]:
+        rez = a + b
+    elif op == operations[0]:
+        rez = a - b
+    elif op == operations[0]:
+        rez = a / b
+    elif op == operations[0]:
+        rez = a * b
+    else:
+        print("Выбрана неверная операция, попробуйте снова")
+        continue
