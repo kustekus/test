b6b501edd1e1eb9ea6bc256da09e0489bade5e8b
kustekus <artdubrovin17@gmail.com>
2023-12-16 22:34:19 +0300

    Ввод чисел и список операций

--- main.py
+++ main.py
@@ -1,16 +1,5 @@
-import random
-while True:
-    a=random.randint(1,100)
-    b=random.randint(1,100)
-    operations=["+","-"]
-    op=random.choice(operations)
-    if op=="+":
-        rez=a+b
-        otv=int(input(f"Чему равно: {a} + {b}? "))
-    else:
-        rez=a-b
-        otv=int(input(f"Чему равно: {a} - {b}? "))
-    if otv==rez:
-        print("Ответ верный!")
-    else:
-        print("Ответ неверный!")
+a=int(input("Введите первое число: "))
+op=input("Введите операцию: ")
+b=int(input("Введите второе число: "))
+operations=["+","-","/","*"]
+
